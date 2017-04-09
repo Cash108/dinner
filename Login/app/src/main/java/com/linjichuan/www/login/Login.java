62bf264f6c99c9bba09bba5a1f0d78f502d14bce
LamKeichuen <14301039@bjtu.edu.cn>
2017-04-09 20:48:19 +0800

    全部更新
    Signed-off-by: LamKeichuen <14301039@bjtu.edu.cn>

---
+++ Login/app/src/main/java/com/linjichuan/www/login/Login.java
@@ -0,0 +1,15 @@
+package com.linjichuan.www.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {/*继承activity类*/
+        super.onCreate(savedInstanceState);        /*调用父类方法*/
+        setContentView(R.layout.activity_login); /*加载布局文件*/
+
+
+    }
+}
